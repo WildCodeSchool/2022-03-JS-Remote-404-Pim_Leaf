04bc5c65f8d68365da0662e36cd7a41f369b4054
Benoît V <benoit@vandanjon.fr>
2022-07-06 15:05:37 +0200

    refacto Products components and change route call on APP

--- frontend/src/pages/common/Products.jsx
+++ frontend/src/pages/common/Products.jsx
@@ -1,13 +1,19 @@
 /* eslint-disable import/no-unresolved */
 import ListProducts from "@retailersC/ListProducts";
+import ProductsTable from "@suppliersC/ProductsTable";
+import { useContext } from "react";
+
+import UserExport from "@contexts/UserContext";
 
 function Products() {
+  const { user } = useContext(UserExport.UserContext);
   return (
     <main>
       <div className="flex justify-center text-3xl font-bold font-barlow mt-20">
         <h1>Mes fiches produits</h1>
       </div>
-      <ListProducts />
+
+      {user.company_group_id === 1 ? <ListProducts /> : <ProductsTable />}
     </main>
   );
 }
