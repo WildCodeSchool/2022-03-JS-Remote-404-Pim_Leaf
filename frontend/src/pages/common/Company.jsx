56fac7e82694003649ff5c99673c32657c830185
Benoît V <benoit@vandanjon.fr>
2022-07-06 20:19:50 +0200

    refacto company page with fields. Lack map on component to reduce

--- frontend/src/pages/common/Company.jsx
+++ frontend/src/pages/common/Company.jsx
@@ -1,22 +1,26 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/no-unresolved */
-import Fields from "@components/common/Fields";
-// import FieldName from "@components/common/FieldName";
-// import FieldEmail from "@components/common/FieldEmail";
-// import FieldPwdOld from "@components/common/FieldPwdOld";
-// import FieldPwdNew from "@components/common/FieldPwdNew";
+
+// import { useForm } from "react-hook-form";
+
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
+
+import FormField from "@components/common/FormField";
 
 import UserExport from "@contexts/UserContext";
 
 function Company() {
   const { user } = useContext(UserExport.UserContext);
+  // const { register, handleSubmit } = useForm();
 
   const [data, setDatas] = useState(null);
+
   useEffect(() => {
     axios
       .get(
-        `${import.meta.env.VITE_BACKEND_URL}company/${user.company_group_id}`
+        // `${import.meta.env.VITE_BACKEND_URL}company/${user.company_group_id}`
+        `${import.meta.env.VITE_BACKEND_URL}company/3`
       )
       .then((res) => {
         setDatas(res.data);
@@ -29,9 +33,39 @@ function Company() {
         Mon entreprise
       </h1>
 
-      <Fields company={data} />
+      <form className="font-redHat flex flex-col min-w-max w-4/5 m-auto">
+        <FormField name="Nom" labels="name" placeholder={data.user_id} />
 
-      {/* <FieldName /> */}
+        <FormField
+          name="Domaine d'activité"
+          labels="domain"
+          placeholder={data.domain}
+        />
+
+        <FormField
+          name="Description"
+          labels="description"
+          placeholder={data.description}
+        />
+
+        <FormField name="Adresse" labels="address" placeholder={data.address} />
+
+        <FormField
+          name="Code postal"
+          labels="postcode"
+          placeholder={data.postcode}
+        />
+
+        <FormField name="Ville" labels="city" placeholder={data.city} />
+
+        <FormField
+          name="Email de contact"
+          labels="email"
+          placeholder={data.company_mail}
+        />
+
+        <FormField name="Téléphone" labels="phone" placeholder={data.phone} />
+      </form>
     </main>
   );
 }
